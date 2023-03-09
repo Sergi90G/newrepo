d01beeed7cab1332ca82a05442fb19620efb842c
sergo gogishvili <sergo.gogishvili.1@iliauni.edu.ge>
2023-03-09 13:09:52 +0400

    second file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    names=["mari","sergi","lika","giga"]
+    return render_template("index.html", list_of_names=names)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
