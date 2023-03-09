45888b8caae4688a14375b7790aff5f0a0a8be33
sergo gogishvili <sergo.gogishvili.1@iliauni.edu.ge>
2023-03-09 18:47:11 +0400

    second file

--- second.py
+++ second.py
@@ -7,6 +7,3 @@ def hello_world():
     names=["mari","sergi","lika","giga"]
     return render_template("index.html", list_of_names=names)
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
